c5b464171cc6a26abfe2be1bcced534f758b005a
HoltonHomes <mr.jacobthekeeper1@gmail.com>
2026-08-30 21:21:23 -0400

    Wire ATLAS query provider and design tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
+import { queryClient } from './lib/queryClient'
+import './design/tokens.css'
 import './styles.css'
 import './parcel.css'
 import './report-polish.css'
@@ -12,6 +15,8 @@ import './phase-two.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
